fbe8b5db2b9f263680f91d3bd168d8bfc92a171e
Chris Young <chrisyoung@Chriss-MacBook-Air.local>
2014-10-26 11:49:19 -0700

    Refactor before hooking up Tanga API

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -50,35 +50,27 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         return cell
     }
+    
     func searchItunesFor(searchTerm: String) {
-        
-        // The iTunes API wants multiple terms separated by + symbols, so replace spaces with + signs
-        let itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
-        
-        // Now escape anything else that isn't URL-friendly
-        if let escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding) {
-            let urlPath = "http://itunes.apple.com/search?term=\(escapedSearchTerm)&media=software"
-            let url: NSURL = NSURL(string: urlPath)!
-            let session = NSURLSession.sharedSession()
-            let task = session.dataTaskWithURL(url, completionHandler: {data, response, error -> Void in
-                if(error != nil) {
-                    // If there is an error in the web request, print it to the console
-                    println(error.localizedDescription)
-                }
-                var err: NSError?
-                
-                var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
-                if(err != nil) {
-                    // If there is an error parsing JSON, print it to the console
-                    println("JSON Error \(err!.localizedDescription)")
-                }
+        let url: NSURL = NSURL(string: "http://itunes.apple.com/search?term=\(searchTerm)&media=software")!
 
-                self.didReceiveAPIResults(jsonResult)
-            })
-            
-            task.resume()
-        }
+        NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: {data, response, error -> Void in
+            if(error != nil) { println(error.localizedDescription) }
+            self.didReceiveAPIResults(self.parse(data))
+        }).resume()
     }
+    
+    func parse(data: NSData) -> NSDictionary {
+        var err: NSError?
+        
+        var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
+        if(err != nil) {
+            // If there is an error parsing JSON, print it to the console
+            println("JSON Error \(err!.localizedDescription)")
+        }
+        return jsonResult
+    }
+    
     func didReceiveAPIResults(results: NSDictionary) {
         var resultsArr: NSArray = results["results"] as NSArray
         dispatch_async(dispatch_get_main_queue(), {
